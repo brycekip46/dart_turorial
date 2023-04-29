07dec7601247ace9f7ba83ac616e40f85abf9730
brycekip46 <brycekip46@gmail.com>
2023-04-30 01:32:07 +0300

    Changes

---
+++ Demo/lib/main1/caseStatement.dart
@@ -0,0 +1,18 @@
+void main (){
+  int option =4;
+  switch(option){
+    case 1:
+      print("You chose one :");
+      break;
+    case 2:
+      print("your option is 2");
+      break;
+    case 3:
+      print("your option is 3");
+      break;
+    default:
+      print("Not a valid option");
+
+  }
+
+}
---
+++ Demo/lib/main1/loopControl.dart
@@ -0,0 +1,20 @@
+void main (){
+  int i=0;
+  for(i=0;i<5;i++){
+    print("A for loop");
+  }
+  i=0;
+  do {
+    print("A do loop");
+    i++;
+
+  }
+  while(i<5);
+i=0;
+  while(i<5){
+print("A while loop");
+i++;
+  }
+
+
+}
