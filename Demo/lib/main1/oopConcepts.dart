dbd5a9c73b54c5dbf7956922c04f89998adc0380
brycekip46 <brycekip46@gmail.com>
2023-04-30 23:11:53 +0300

    OOP concepts

---
+++ Demo/lib/main1/oopConcepts.dart
@@ -0,0 +1,24 @@
+class Vehicle{
+  String? type;
+  String?  model;
+
+  Vehicle(){
+    print("Calls the default constructor");
+  }
+
+  void move(){
+
+  }
+  void makeNoise(){
+
+  }
+  void transmission(){
+
+  }
+
+}
+void main(){
+  Vehicle v1 = Vehicle();
+  v1.type="Mercedes";
+
+}
