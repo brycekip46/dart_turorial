36f12dfd99316e7a7e1d61fdfec1442b55e7afbb
kiprutobrysonn <brycekip46@gmail.com>
2023-05-12 15:05:08 +0300

    Collections

--- Demo/lib/main1/Collections.dart
+++ Demo/lib/main1/Collections.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
-//Collections define types of data elemets that can be used to store multiple elements
-//First type: Array A contigous storage location> Also a list
+//Collections define types of data elements that can be used to store multiple elements
+//First type: Array A contiguous storage location> Also a list
 //Type:Fixed Range
 void main (){
   var marks =List.filled(7, 0, growable: false);
@@ -19,18 +19,40 @@ void main (){
   }
  marks.forEach((element) =>print(element));
   //Type:Growable List
-  //Creates a dynamic list that can be continuously appendeded
+  //Creates a dynamic list that can be continuously appended
 var marks2 = [];
 marks2.add(3);
 
 //Type:Set and HashSet
 //A Set defines a collection of unordered elements 
 //Items are not stored in an indexed location
-   var names = {"James","Ricky","Devansh","Adam"};   
+   var names = {"James","Ricky","Devansh","Devan"};
    // Declaring empty set  
    var emp = <String>{};  
    emp.add("Jonathan");
-   e
+   emp.add(names.elementAt(0));
+  emp.add(names.elementAt(1));
+  emp.add(names.elementAt(2));
+  emp.add(names.elementAt(3));
+
+  emp.forEach((element) => print(element));
+  var emp2 = <String>{"Joash","John","Devan"};
+//Set operations :union:intersection;difference;
+  print(emp.union(emp2));
+
+  print(emp.intersection(emp2));
+  //It deletes all elements
+  emp.clear();
+  if(emp.isEmpty){
+    print("This is an empty set");
+  }
+  //MAp defines an object that stores data in key value pair
+  var passUser ={"Devan":"123","Joash":"3900"};
+  passUser["John"]="3456";
+  print(passUser.keys);
+  print(passUser.length);
+  passUser.remove("Joash");
+
 
 
 }
